960e0a9fb12708b9ded130e94e34090f58cbb6bb
Woo Song <ws392@drexel.edu>
2020-05-26 02:36:12 -0400

    adding some fake data

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -7,17 +7,58 @@ class Album extends Component {
   render() {
     return (
       <AlbumContainer id="album-page">
-        <section className="page-section selected-album-section bg--neutral">
-          <TwoColumnHero fullImage={true} />
+        <section className="page-section selected-album-section bg--green">
+          <TwoColumnHero
+            title="Comrade K.K."
+            description="The song takes influence from traditional Russian folk music and especially Bard music, a genre that was common among amateur musicians in the former Soviet Union."
+            image="https://vignette.wikia.nocookie.net/animalcrossing/images/6/67/NH-Album_Cover-Comrade_K.K..png/revision/latest?cb=20200502232547"
+            btnText="Save on Spotify"
+          />
         </section>
         <section className="page-section album-grid-section">
           <Grid colClass="three-col">
-            <Card tag="" noText={true} />
-            <Card tag="" noText={true} />
-            <Card tag="" noText={true} />
-            <Card tag="" noText={true} />
-            <Card tag="" noText={true} />
-            <Card tag="" noText={true} />
+            <Card
+              tag=""
+              description=""
+              title="Comrade K.K."
+              image="https://vignette.wikia.nocookie.net/animalcrossing/images/6/67/NH-Album_Cover-Comrade_K.K..png/revision/latest?cb=20200502232547"
+              noText={true}
+            />
+            <Card
+              tag=""
+              description=""
+              title="DJ K.K."
+              image="https://vignette.wikia.nocookie.net/animalcrossing/images/7/7a/NH-Album_Cover-DJ_K.K..png/revision/latest?cb=20200502232717"
+              noText={true}
+            />
+            <Card
+              tag=""
+              description=""
+              title="Drivin"
+              image="https://vignette.wikia.nocookie.net/animalcrossing/images/0/0b/NH-Album_Cover-Drivin%27.png/revision/latest?cb=20200502233001"
+              noText={true}
+            />
+            <Card
+              tag="Top Hit"
+              description=""
+              title="Aloha"
+              image="https://vignette.wikia.nocookie.net/animalcrossing/images/5/5d/NH-Album_Cover-Aloha_K.K..png/revision/latest?cb=20200502231742"
+              noText={true}
+            />
+            <Card
+              tag=""
+              description=""
+              title="Agent K.K."
+              image="https://vignette.wikia.nocookie.net/animalcrossing/images/9/91/NH-Album_Cover-Agent_K.K..png/revision/latest?cb=20200502231523"
+              noText={true}
+            />
+            <Card
+              tag=""
+              description=""
+              title="Bubblegum"
+              image="https://vignette.wikia.nocookie.net/animalcrossing/images/e/e6/NH-Album_Cover-Bubblegum_K.K..png/revision/latest?cb=20200502232328"
+              noText={true}
+            />
           </Grid>
 
           <div className="more-btn-wrapper o-container">
@@ -30,6 +71,18 @@ class Album extends Component {
 }
 
 const AlbumContainer = styled.div`
+  .album-grid-section {
+    .c-card__tag-wrapper {
+      background-color: var(--kk-orange);
+    }
+
+    .c-card__link {
+      &:hover {
+        box-shadow: 3px 3px var(--kk-white), 5px 5px var(--kk-orange);
+      }
+    }
+  }
+
   .more-btn-wrapper {
     display: flex;
     flex-direction: column;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { TwoColumnHero, Slideshow, Button } from "kk-design-system";
+import { TwoColumnHero, Button } from "kk-design-system";
 import styled from "styled-components";
 
 class Home extends Component {
@@ -8,17 +8,29 @@ class Home extends Component {
     return (
       <HomeContainer id="home-page">
         <section className="page-section featured-section">
-          <Slideshow>
-            <TwoColumnHero />
-            <TwoColumnHero />
-          </Slideshow>
+          <h2 className="section-title u-sr-only">Featured album</h2>
+          <TwoColumnHero
+            title="To the Edge"
+            description=""
+            image="https://vignette.wikia.nocookie.net/animalcrossing/images/5/52/NH-Album_Cover-To_the_Edge.png/revision/latest?cb=20200503003411"
+          />
         </section>
-        <section className="page-section more-section">
-          <TwoColumnHero reverseOrder={true} />
-          <TwoColumnHero />
-          <div className="more-btn-wrapper o-container">
-            <Button>Save on Spotify</Button>
-            <Button>Save on Spotify</Button>
+        <section className="page-section more-section bg--blue">
+          <div className="o-container">
+            <h2 className="section-title u-sr-only">Featured artists</h2>
+          </div>
+          <TwoColumnHero
+            reverseOrder={true}
+            title="T-Bone (ボルシチ, Borushichi)"
+            description="He is named after the cut of beef, t-bones. His Japanese name is the name of a Ukrainian dish, borscht, which sometimes uses beef or beef bone as stock."
+            image="https://vignette.wikia.nocookie.net/animalcrossing/images/0/09/TBone_NewLeaf_Official.png/revision/latest?cb=20130721212940"
+          />
+          <TwoColumnHero
+            image="https://vignette.wikia.nocookie.net/animalcrossing/images/c/cd/Doc_NH.png/revision/latest?cb=20200322125229"
+            title="Doc (トビオ, Tobio)"
+            description="His name may reference the famous Bugs Bunny quote, 'Eh, what's up doc?'. His name and likeness may also be based on Dr. Wright from the SNES version of SimCity."
+          />
+          <div className="more-btn-wrapper">
             <Button>Save on Spotify</Button>
           </div>
         </section>
@@ -28,14 +40,24 @@ class Home extends Component {
 }
 
 const HomeContainer = styled.div`
+  .featured-section {
+    .c-two-col-hero__title {
+      font-size: 5rem;
+    }
+  }
+
   .more-section {
-    & > *:not(.more-btn-wrapper) {
+    .c-two-col-hero {
       height: auto;
       margin-bottom: 3rem;
 
       @media only screen and (min-width: 768px) {
         height: 30rem;
       }
+    }
+
+    .c-two-col-hero img {
+      object-fit: contain;
     }
   }
 
