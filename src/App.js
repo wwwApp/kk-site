ca711389bd0912b4fd38ed3c7f5fb9e35a23d1c3
Woo Song <ws392@drexel.edu>
2020-06-02 01:17:55 -0400

    trying to fix router issue

--- src/App.js
+++ src/App.js
@@ -2,24 +2,22 @@ import React from "react";
 import Home from "./pages/Home";
 import Album from "./pages/Album";
 import styled from "styled-components";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <AppContainer className="App">
-      <Router>
-        <Header />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/albums">
-            <Album />
-          </Route>
-        </Switch>
-      </Router>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/albums">
+          <Album />
+        </Route>
+      </Switch>
       <Footer />
     </AppContainer>
   );
